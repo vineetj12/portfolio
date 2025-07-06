827e5c0624ec755a688c3953b9f76cf1a794e8fa
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-06 16:32:30 +0530

    add

--- projectectdata/data.tsx
+++ projectectdata/data.tsx
@@ -12,5 +12,17 @@ const cards = [
         className: "h-[300px]",
        thumbnail: "/picture/ai-interview.png",
     },
+    {id: 2,
+      label: "AI Interviewer",
+      url:"https://ai-interviewer-xi-indol.vercel.app/",
+      content: (
+        <div className="text-white">
+        <h2 className="text-xl font-bold">AI Interview Assistant</h2>
+        <p>An AI-powered mock interview tool with dynamic questions, feedback, and scoring.</p>
+        </div>
+        ),
+        className: "h-[300px]",
+       thumbnail: "/picture/ai-interview.png",
+    }
   ];
 export default cards;
