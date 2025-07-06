d8fbb6d6ffd16c654098002800e1abe52d410327
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-06 14:06:24 +0530

    picture of second project and url

--- projectectdata/data.tsx
+++ projectectdata/data.tsx
@@ -12,19 +12,6 @@ const cards = [
     className: "h-[300px]",
     thumbnail: "/picture/ai-interview.png",
   },
-  {
-    id: 2,
-    label: "URL Shortener",
-    url: "https://shorturl-ten-mocha.vercel.app/",
-    content: (
-      <div className="text-white">
-        <h2 className="text-xl font-bold">URL Shortener</h2>
-        <p>A simple and fast tool to shorten long URLs, track clicks, and manage links.</p>
-      </div>
-    ),
-    className: "h-[300px]",
-    thumbnail: "/picture/url.png",
-  },
 ];
 
 export default cards;
